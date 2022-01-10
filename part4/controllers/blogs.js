d1a14ec5b23dac5151cdf31a9273eb20ef8ff2ab
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-01-10 13:35:06 -0700

    part4d

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,46 +1,62 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.js");
-const User = require('../models/user')
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1 });
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   console.log(body);
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
 
-  const blog = new Blog({ 
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id
+    user: user._id,
   });
-  
+
   const saved = await blog.save();
   user.blogs = user.blogs.concat(saved._id);
-  await user.save()
+  await user.save();
   response.json(saved);
 });
 
-blogsRouter.delete("/:id", async(request, response)=>{
+blogsRouter.delete("/:id", async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id);
 
-  response.status(204).end()
+  response.status(204).end();
 });
 
-blogsRouter.put("/:id", async(request, response)=>{
-  const req = request.body
-  const newBlog ={
-    likes : req.likes
+blogsRouter.put("/:id", async (request, response) => {
+  const req = request.body;
+  const newBlog = {
+    likes: req.likes,
   };
 
-  const blog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {new:true})
+  const blog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {
+    new: true,
+  });
 
   response.json(blog);
-})
+});
 module.exports = blogsRouter;
