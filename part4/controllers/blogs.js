8ee1111279a578da895ce08ec83d484580cbdace
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-01-10 11:23:50 -0700

    part 4c

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,16 +1,29 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.js");
+const User = require('../models/user')
+
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
-  console.log(request.body);
-  const saved = await blog.save();
+  const body = request.body;
+  console.log(body);
+  const user = await User.findById(body.userId)
 
+  const blog = new Blog({ 
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  });
+  
+  const saved = await blog.save();
+  user.blogs = user.blogs.concat(saved._id);
+  await user.save()
   response.json(saved);
 });
 
